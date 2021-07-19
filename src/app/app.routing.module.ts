1b1ef94cf42ec54e7beb13758a336a1d56ebe0fd
fkhan <faisal.khan@telenetgroup.be>
2021-07-19 23:04:39 +0200

    auth guard

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth.guard";
 import { LoginComponent } from "./login/login.component";
+import { TodoComponent } from "./todo/todo.component";
 
 const routes: Routes = [
     {
-        path: '', component: LoginComponent
+        path: '', component: TodoComponent,
+        canActivate: [AuthGuard]
     }
 ]
 @NgModule({
